58bdb628ddf9bf892de6a06018cac377efc24c02
Andrey Guzyuk <necromant1k96@gmail.com>
2019-07-19 18:03:12 +0300

    Fix header position

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,56 +29,58 @@ const SectionCellGroup = styled.div`
 `
 const IndexPage = () => (
   <Layout>
-  <div>
-   <div className="Hero">
-        <h1>Alpha + Omega digital </h1>
-        <p>Build. Brand. Develop </p>
-        <Link to="/page-2/">Watch the video</Link>
-        <div className="Logos">
-        <img src={require("../images/logo-sketch.png")} width="50" />
-          <img src={require("../images/logo-figma.png")} width="50" />
-          <img src={require("../images/logo-framer.png")} width="50" />
-          <img src={require("../images/logo-react.png")} width="50" />
-          <img src={require("../images/logo-swift.png")} width="50" />
-          <img src={require("../images/logo-studio.png")} width="50" />
-        <Wave />
+    <div>
+      <div className="Hero">
+        <div className="HeroGroup">
+          <h1>Alpha + Omega digital </h1>
+          <p>Build. Brand. Develop </p>
+          <Link to="/page-2/">Watch the video</Link>
+          <div className="Logos">
+            <img src={require("../images/logo-sketch.png")} width="50" />
+            <img src={require("../images/logo-figma.png")} width="50" />
+            <img src={require("../images/logo-framer.png")} width="50" />
+            <img src={require("../images/logo-react.png")} width="50" />
+            <img src={require("../images/logo-swift.png")} width="50" />
+            <img src={require("../images/logo-studio.png")} width="50" />
+            <Wave />
+          </div>
+        </div>
       </div>
+      <div className="Cards">
+        <h2>See all Projects</h2>
+        <div className="CardGroup">
+          <Card
+            title="In and Out Fitness"
+            text="12 sections"
+            image={require('../images/wallpaper.jpg')}
+          />
+          <Card
+            title="Gutter Guard"
+            text="12 sections"
+            image={require('../images/wallpaper2.jpg')}
+          />
+          <Card
+            title="Mercers restaurant"
+            text="12 sections"
+            image={require('../images/wallpaper3.jpg')}
+          />
+        </div>
       </div>
-    <div className="Cards">
-      <h2>See all Projects</h2>
-      <div className="CardGroup">
-        <Card
-          title="In and Out Fitness"
-          text="12 sections"
-          image={require('../images/wallpaper.jpg')}
-        />
-        <Card
-          title="Gutter Guard"
-          text="12 sections"
-          image={require('../images/wallpaper2.jpg')}
-        />
-        <Card
-          title="Mercers restaurant"
-          text="12 sections"
-          image={require('../images/wallpaper3.jpg')}
-        />
-      </div>
-    </div>
-    <Section
-     image= {require('../images/wallpaper2.jpg')}
-     logo= {require('../images/logo-react.png')}
-      title="React for Designers"
-      text="Learn how to build a modern site using React and the most efficient libraries to get your site/product online. Get familiar with components, Grid CSS, animations, interactions, dynamic data with Contentful and deploying your site with Netlify."
-     />
-    <SectionCaption>12 sections - 6 hours</SectionCaption>
-  <SectionCellGroup>
-  {staticdata.cells.map(cell => (
-  <Cell
-     title={cell.title}
-     image={cell.image} />
-      ))}
+      <Section
+        image= {require('../images/wallpaper2.jpg')}
+        logo= {require('../images/logo-react.png')}
+        title="React for Designers"
+        text="Learn how to build a modern site using React and the most efficient libraries to get your site/product online. Get familiar with components, Grid CSS, animations, interactions, dynamic data with Contentful and deploying your site with Netlify."
+      />
+      <SectionCaption>12 sections - 6 hours</SectionCaption>
+      <SectionCellGroup>
+        {staticdata.cells.map(cell => (
+          <Cell
+            title={cell.title}
+            image={cell.image} />
+        ))}
       </SectionCellGroup>
-      </div>
+    </div>
  </Layout>
 )
 
