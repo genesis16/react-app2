cdfa05448b9ace145bfef301145db7f960474a74
genesis16 <janeleeshian@gmail.com>
2019-07-19 22:21:38 +1000

    third commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,44 +32,48 @@ const IndexPage = () => (
   <div>
     <div className="Hero">
       <div className="HeroGroup">
-        <h1>Learn to <br />design and code React apps</h1>
-        <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
+        <h1>Alpha + Omega digital </h1>
+        <p>Build. Brand. Develop </p>
         <Link to="/page-2/">Watch the video</Link>
         <div className="Logos">
-          <img src='./images/logo-sketch' width="50" />
-          <img src='/images/logo-figma.png' width="50" />
-          <img src='/images/logo-studio.png' width="50" />
-          <img src='/images/logo-framer.png' width="50" />
-          <img src='/images/logo-react.png' width="50" />
-          <img src='/images/logo-swift.png' width="50" />
+        <img src={require("../images/logo-sketch.png")} width="50" />
+          <img src={require("../images/logo-figma.png")} width="50" />
+          <img src={require("../images/logo-framer.png")} width="50" />
+          <img src={require("../images/logo-react.png")} width="50" />
+          <img src={require("../images/logo-swift.png")} width="50" />
+          <img src={require("../images/logo-studio.png")} width="50" />
         </div>
         <Wave />
       </div>
     </div>
     <div className="Cards">
-      <h2>11 courses, more coming</h2>
+      <h2>See all Projects</h2>
       <div className="CardGroup">
-        <Card 
-          title="Design System"
-          text="10 sections"
-          image='/images/wallpaper.jpg' />
-        <Card 
-          title="React for Designers"
+        <Card
+          title="In and Out Fitness"
           text="12 sections"
-          image='/images/wallpaper2.jpg' />
-        <Card 
-          title="Sound Design"
-          text="5 sections"
-          image='/images/wallpaper3.jpg' />
-        <Card 
-          title="ARKit 2"
-          text="10 sections"
-          image='/images/wallpaper4.jpg' />
+          image={require('../images/wallpaper.jpg')}
+        />
+        <Card
+          title="Gutter Guard"
+          text="12 sections"
+          image={require('../images/wallpaper2.jpg')}
+        />
+        <Card
+          title="Mercers restaurant"
+          text="12 sections"
+          image={require('../images/wallpaper3.jpg')}
+        />
+        <Card
+          title="Eltham Chiro"
+          text="12 sections"
+          image={require('../images/wallpaper4.jpg')}
+        />
       </div>
     </div>
     <Section
-      image='/images/wallpaper2.jpg'
-      logo='/images/logo-react.png'
+     image= {require('../images/wallpaper2.jpg')} 
+     logo= {require('../images/logo-react.png')}
       title="React for Designers"
       text="Learn how to build a modern site using React and the most efficient libraries to get your site/product online. Get familiar with components, Grid CSS, animations, interactions, dynamic data with Contentful and deploying your site with Netlify."
      />
